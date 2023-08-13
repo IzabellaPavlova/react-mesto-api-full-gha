93f33a8cef397e19f1ac960c9f9717a68e743379
Izabella Pavlova <izabella1612@mail.ru>
2023-08-13 16:26:11 +0300

    add env

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,9 +1,9 @@
-const MONGO_URL = process.env.PORT || 'mongodb://localhost:27017/mestodb';
-const PORT = process.env.PORT || 3000;
-const JWT_SECRET = 'dev-secret';
+const { PORT = 3000 } = process.env;
+const { MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const {JWT_SECRET = 'secret'} = process.env;
 
 module.exports = {
-  MONGO_URL,
   PORT,
-  JWT_SECRET,
+  MONGO_URL,
+  JWT_SECRET
 };
