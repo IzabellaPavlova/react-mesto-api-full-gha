b337bd9cbd3b31edc103306980f430087371dad7
Izabella Pavlova <izabella1612@mail.ru>
2023-08-13 12:03:56 +0300

    backend cors

--- backend/app.js
+++ backend/app.js
@@ -3,29 +3,36 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const ddos = require('./middlewares/ddos');
 const errorCatcher = require('./middlewares/errorCatcher');
 const { requestsLogger, errorsLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
 const routes = require('./routes');
 const { PORT, MONGO_URL } = require('./utils/config');
 
 const app = express();
 mongoose.connect(MONGO_URL);
 
+app.use(cors({
+  origin: [
+    'http://localhost:3001',
+    'https://feierabend.nomoreparties.co',
+  ],
+  credentials: true,
+  maxAge: 30,
+}));
 app.use(helmet());
 app.use(ddos);
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors);
 app.use(requestsLogger);
 
 // TODO: Delete after review
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
 app.use(routes);
 
