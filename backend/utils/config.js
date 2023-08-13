8d7efd82e9c99db545240cfe534da03866186af7
Izabella Pavlova <izabella1612@mail.ru>
2023-08-13 16:30:00 +0300

    fix lint

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,9 +1,9 @@
 const { PORT = 3000 } = process.env;
 const { MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-const {JWT_SECRET = 'secret'} = process.env;
+const { JWT_SECRET = 'secret' } = process.env;
 
 module.exports = {
   PORT,
   MONGO_URL,
-  JWT_SECRET
+  JWT_SECRET,
 };
